e6c7d8ee41f2fad84e23f42e940ec0ea7780c4e5
CiprianUngur <59055433+CiprianUngur@users.noreply.github.com>
2020-01-12 13:06:53 +0200

    Update inch.java

--- src/tema/inch.java
+++ src/tema/inch.java
@@ -2,9 +2,14 @@ package tema;
 
 public class inch {
     public static void main(String[] args) {
+        inches();
+    }
+
+    public static void inches(){
         int inch = 27;
         double y = 0.0254;
         double meters = inch * y;
         System.out.println(inch+" inches are "+meters+" meters");
+
     }
 }
