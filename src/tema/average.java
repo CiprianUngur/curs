06f25f3ea9b30be11468582d7bd853e8a13e0c73
CiprianUngur <59055433+CiprianUngur@users.noreply.github.com>
2020-01-12 13:20:27 +0200

    Update average.java

--- src/tema/average.java
+++ src/tema/average.java
@@ -2,8 +2,7 @@ package tema;
 
 public class average {
     public static void main(String[] args) {
-        double x = avg(21, 56, 44);
-        System.out.println(x);
+        System.out.println("The result of the average is: " + avg(21, 56, 44));
     }
     public static double avg(double first, double second, double third){
         double avg = (first + second + third) / 3;
