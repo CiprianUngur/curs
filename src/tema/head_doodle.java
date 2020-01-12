9c410df040e909676afb3ff7df4ff669c7bdbaf2
CiprianUngur <59055433+CiprianUngur@users.noreply.github.com>
2020-01-12 13:03:29 +0200

    Update head_doodle.java

--- src/tema/head_doodle.java
+++ src/tema/head_doodle.java
@@ -2,6 +2,9 @@ package tema;
 
 public class head_doodle {
     public static void main(String[] args) {
+    JavaDoodle();
+    }
+    public static void JavaDoodle(){
         System.out.println("  +\"\"\"\"\"+");
         System.out.println("[ | o o | ]");
         System.out.println("  |  ^  |");
