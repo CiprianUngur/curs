ff412b48546474c4ebc3785aba898664e64a9e98
CiprianUngur <59055433+CiprianUngur@users.noreply.github.com>
2020-01-12 13:30:57 +0200

    Update java_doodle.java

--- src/tema/java_doodle.java
+++ src/tema/java_doodle.java
@@ -3,11 +3,13 @@ package tema;
 public class scheme {
 
     public static void main(String[] args){
+        printThatScheme();
+    }
+    public static void printThatScheme(){
         System.out.println("   J    a   v     v   a");
         System.out.println("   J   a a   v   v   a a");
         System.out.println("J  J  aaaaa   V V   aaaaa");
         System.out.println(" JJ  a     a   V   a     a");
-    }
-
+}
 
 }
